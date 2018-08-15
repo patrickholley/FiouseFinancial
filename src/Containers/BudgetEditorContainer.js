f87e17367b783200e140b01f7c1e7330caf63047
Patrick Holley <patrick_holley@outlook.com>
2018-08-15 11:34:34 -0600

    added budget class, basic logic for handling invalid balances

--- src/Containers/BudgetEditorContainer.js
+++ src/Containers/BudgetEditorContainer.js
@@ -6,6 +6,7 @@ import PropTypes from 'prop-types';
 import BudgetEditorPresentation from '../Presentations/BudgetEditorPresentation';
 import { LOGOUT_REQUEST } from '../constants/actions';
 import allFormsValues from '../constants/allFormsValues';
+import Budget from '../classes/Budget';
 
 class BudgetEditorContainer extends React.Component {
   lengthTypes = [
@@ -48,7 +49,28 @@ class BudgetEditorContainer extends React.Component {
   }
 
   onFormSubmit = () => {
-    console.log(this.state.formValues);
+    const { fields } = this.state.formValues;
+    const balance = fields.balance.value;
+
+    if (isNaN(balance)) { // eslint-disable-line no-restricted-globals
+      // error handling here
+      console.log('Not a number!');
+    } else if (balance > 999999999) {
+      // error handling here
+      console.log('Too big!');
+    } else {
+      const reformattedBalance = parseFloat(balance).toFixed(2);
+
+      const myBudget = new Budget(
+        'new',
+        'test@test.com',
+        fields.name.value,
+        fields.lengthType.value,
+        reformattedBalance,
+      );
+
+      console.log(myBudget);
+    }
   }
 
   generateLengthTypeItems = () => {
---
+++ src/classes/Budget.js
@@ -0,0 +1,9 @@
+export default class Budget {
+  constructor(id, userId, name, lengthTypeId, balance) {
+    this.id = id;
+    this.userId = userId;
+    this.name = name;
+    this.lengthTypeId = lengthTypeId;
+    this.balance = balance;
+  }
+}
